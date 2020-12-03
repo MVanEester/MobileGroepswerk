9f55f27f6c1f47a3e1c6b6bb541d709fea2da712
MelvinGoorden <43994641+MelvinGoorden@users.noreply.github.com>
2020-12-03 16:07:57 +0100

    working markers

--- EvaluatieMobile/Components/MapPage.js
+++ EvaluatieMobile/Components/MapPage.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker, annotations } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions,  } from 'react-native';
 
 const getData = () => {
    fetch('https://opendata.arcgis.com/datasets/99c7168df28142958cbfec31cd633d56_289.geojson')
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((json) => {
       console.log(json);
+      return json
     })
     .catch((error) => {
       console.error(error);
@@ -16,27 +17,53 @@ const getData = () => {
 // console.log(jsondata);
 
 const MapPage = () => {
-  let jsondata;
+  const [markers, setMarkers] = useState([]);
+  // let markers = [];
   useEffect(() => {
-    jsondata = getData();
+    fetch('https://opendata.arcgis.com/datasets/99c7168df28142958cbfec31cd633d56_289.geojson')
+    .then((response) => response.json())
+    .then((json) => {
+      // console.log(json);
+      let marker = []
+      json.features.map(feature => {
+        console.log();
+        marker.push({
+          key: feature.properties.OBJECTID,
+          title: feature.properties.NAAM,
+          latitude: feature.geometry.coordinates[1],
+          longitude: feature.geometry.coordinates[0]
+        })
+      });
+      setMarkers(marker);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }, []);
+  // console.log("marker: ",markers);
 
-  var markers = jsondata.features.map(feature => {
-    feature.geometry.coordinates[0]
-    feature.geometry.coordinates[1]
-  })
   return (
     <View style={styles.container}>
         <MapView
-            style={styles.mapStyle}
-            initialRegion={{
-                latitude: 51.2127037,
-                longitude: 4.409325,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
-            
-        />
+          style={styles.mapStyle}
+          mapType="satellite"
+          initialRegion={{
+              latitude: 51.2127037,
+              longitude: 4.409325,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            {markers.map(marker => (
+               <MapView.Marker 
+                 key={marker.key}
+                 coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+                 title={marker.title}
+               />
+            ))}
+        
+        </MapView>
+
     </View>
   );
 }

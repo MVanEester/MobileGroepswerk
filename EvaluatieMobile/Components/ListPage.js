096f1398daebaf69019dac4bfb69763243c7aabc
Van Eester Michiel <michielvaneesterschool@gmail.com>
2020-12-03 17:12:50 +0100

    lijst page work in progress

--- EvaluatieMobile/Components/ListPage.js
+++ EvaluatieMobile/Components/ListPage.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Dimensions,  } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const ListPage = (props) => {
   const [data, setData] = useState([]);
@@ -7,9 +10,44 @@ const ListPage = (props) => {
     setData(props.data)
   }, [])
   return (
-    <View style={styles.container}>
-        <Text>listapge</Text>
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="buurtfietsenstallingen" 
+        children={() => (
+          <ScrollDetail data={data}/>
+        )}/>
+      <Stack.Screen 
+        name="Detail" 
+        component={DetailPage} options={{title: ''}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+const ScrollDetail = (props) =>{
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    setData(props.data)
+  }, [])
+  return(
+    <View>
+      <ScrollView>
+          {data.map((feature) => {
+            <View key={index}>
+              <Button data={feature} onPress={() => navigation.navigate('Detail', {data: feature})}></Button>
+            </View>
+          })}
+        </ScrollView>
     </View>
+  );
+}
+
+const DetailPage = ({route}) =>{
+  const color = route.params.data
+  return(
+    <View>
+
+  </View>
   );
 }
 

5bc8ee75f784611a2a8075b9a32265a8b02e2989
Van Eester Michiel <michielvaneesterschool@gmail.com>
2020-12-03 14:00:46 +0100

    Navigation

--- EvaluatieMobile/App.js
+++ EvaluatieMobile/App.js
@@ -1,13 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome, FontAwesome5 } from '@expo/vector-icons'; 
+
+const Tab = createBottomTabNavigator();
+
+const Map = () => {
+  return (
+    <View>
+      <Text>dit is de map....</Text>
+    </View>
+  );
+}
+
+const Lijst = () => {
+  return (
+    <View>
+      <Text>dit is de lijst....</Text>
+    </View>
+  );
+}
+
+const Fav = () => {
+  return (
+    <View>
+      <Text>dit zijn de Favorieten....</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen 
+            name="Map" 
+            component={Map}
+            options={{tabBarIcon: ({color, size}) => (
+              <FontAwesome5 name="map" size={24} color="black" />
+              )}
+            } 
+          />
+          <Tab.Screen 
+            name="Lijst" 
+            component={Lijst}
+            options={{tabBarIcon: ({color, size}) => (
+              <FontAwesome name="list-ul" size={24} color="black" />
+              )}
+            } 
+          />
+          <Tab.Screen 
+            name="Favorieten" 
+            component={Fav}
+            options={{tabBarIcon: ({color, size}) => (
+              <FontAwesome name="star" size={24} color="black" />
+              )}
+            } 
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 

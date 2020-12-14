617ba2fd7ba9e4c6f2010f77de14172d62d2f1ec
Van Eester Michiel <michielvaneesterschool@gmail.com>
2020-12-14 17:04:06 +0100

    Favorieten page css updated
    text "geen favo gevonden" css

--- EvaluatieMobile/Components/FavoritePage.js
+++ EvaluatieMobile/Components/FavoritePage.js
@@ -66,7 +66,7 @@ const ScrollDetail = (props) => {
     <View>
       <ScrollView>
         {notFound ?
-        <Text>no favorites found</Text>:
+        <Text style={styles.textStyle}>Geen favorieten gevonden.</Text>:
         (data.map((feature) => {
           return (<View key={feature.key} style={{ marginTop: 2 }}>
             <Button title={feature.title} onPress={() => navigation.navigate('Detail', { data: feature })}>{feature.address}</Button>
@@ -94,5 +94,10 @@ const styles = StyleSheet.create({
 
   itemStyle: {
     fontWeight: "bold",
+  },
+
+  textStyle: {
+    textAlign: 'center',
+    margin: 100,
   },
 });
